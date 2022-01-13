4291f37ba8036dd0f0786eb07a96104757660657
jekatigr <jekatigr@users.noreply.github.com>
2022-01-13 19:02:29 +0000

    docs update

--- packages/ui-helpers/src/index.ts
+++
@@ -1,8 +0,0 @@
-export { default as cnCreate } from './cnCreate';
-export { default as convert } from './convert';
-export { default as detectTouch } from './detectTouch';
-export { default as filterDataAttrs } from './filterDataAttrs';
-export { default as breakpoints } from './breakpoints';
-
-export type { ConvertTransformConfig } from './convert';
-export type { IFilterDataAttrs } from './filterDataAttrs';
